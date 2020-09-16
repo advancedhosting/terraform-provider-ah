2cacef86a91564041ee4a43dbc326cb09f0f94d9
punyabit <punyabit@abs.com>
2020-09-16 16:25:23 +0300

    Implement ah_cloud_server_snapshot

---
+++ ah/resource_ah_cloud_server_snapshot.go
@@ -0,0 +1,233 @@
+package ah
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/advancedhosting/advancedhosting-api-go/ah"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceAHCloudServerSnapshot() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAHCloudServerSnapshotCreate,
+		Read:   resourceAHCloudServerSnapshotRead,
+		Update: resourceAHCloudServerSnapshotUpdate,
+		Delete: resourceAHCloudServerSnapshotDelete,
+		Schema: map[string]*schema.Schema{
+			"cloud_server_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: func() (interface{}, error) { return time.Now().Format("2006-01-02 at 15:04:05"), nil },
+			},
+			"cloud_server_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"size": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceAHCloudServerSnapshotCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ah.APIClient)
+
+	instanceID := d.Get("cloud_server_id").(string)
+	note := d.Get("name").(string)
+
+	_, err := client.Instances.CreateBackup(context.Background(), instanceID, note)
+
+	if err != nil {
+		return fmt.Errorf("Error creating backup: %s", err)
+	}
+
+	//d.SetId(volume.ID) // TODO get backup id after WCS-3600
+
+	// ------------------------------------
+	//TEMPORARY MOCK. ONLY FOR TEST. REMOVE AFTER WCS-3600
+	options := &ah.ListOptions{
+		Filters: []ah.FilterInterface{
+			&ah.EqFilter{
+				Keys:  []string{"instance_id"},
+				Value: instanceID,
+			},
+		},
+	}
+	instanceBackups, err := client.Backups.List(context.Background(), options)
+	if err != nil {
+		return err
+	}
+	backupID := instanceBackups[0].Backups[0].ID
+	d.SetId(backupID)
+	// -------------------------------------
+
+	if err := waitForBackupReady(d, meta); err != nil {
+		return fmt.Errorf(
+			"Error waiting for backup (%s) to become ready: %s", d.Id(), err)
+	}
+
+	return resourceAHCloudServerSnapshotRead(d, meta)
+
+}
+
+func resourceAHCloudServerSnapshotRead(d *schema.ResourceData, meta interface{}) error {
+	backup, instanceName, err := snapshotInfo(d, meta)
+	if err != nil {
+		return err
+	}
+
+	d.Set("cloud_server_id", backup.InstanceID)
+	d.Set("cloud_server_name", instanceName)
+	d.Set("name", backup.Note)
+	d.Set("state", backup.Status)
+	d.Set("size", backup.Size)
+	d.Set("type", backup.Type)
+	d.Set("created_at", backup.CreatedAt)
+
+	return nil
+}
+
+func snapshotInfo(d *schema.ResourceData, meta interface{}) (*ah.Backup, string, error) {
+	client := meta.(*ah.APIClient)
+	options := &ah.ListOptions{
+		Filters: []ah.FilterInterface{
+			&ah.EqFilter{
+				Keys:  []string{"id"},
+				Value: d.Id(),
+			},
+		},
+	}
+	instanceBackups, err := client.Backups.List(context.Background(), options)
+	if err != nil {
+		return nil, "", err
+	}
+
+	if len(instanceBackups) != 1 {
+		return nil, "", ah.ErrResourceNotFound
+	}
+
+	instanceBackup := instanceBackups[0]
+	return &instanceBackup.Backups[0], instanceBackup.InstanceName, nil
+
+}
+
+func resourceAHCloudServerSnapshotUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ah.APIClient)
+	d.Partial(true)
+
+	if d.HasChange("name") {
+
+		updateRequest := &ah.BackUpUpdateRequest{
+			Note: d.Get("name").(string),
+		}
+
+		if _, err := client.Backups.Update(context.Background(), d.Id(), updateRequest); err != nil {
+			return fmt.Errorf(
+				"Error changing backup name (%s): %s", d.Id(), err)
+		}
+		d.SetPartial("name")
+	}
+
+	return resourceAHCloudServerSnapshotRead(d, meta)
+}
+
+func resourceAHCloudServerSnapshotDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ah.APIClient)
+	if _, err := client.Backups.Delete(context.Background(), d.Id()); err != nil {
+		return fmt.Errorf(
+			"Error deleting backup (%s): %s", d.Id(), err)
+	}
+	if err := waitForBackupDestroy(d, meta); err != nil {
+		return fmt.Errorf(
+			"Error waiting for backup (%s) to become destroyed: %s", d.Id(), err)
+	}
+	return nil
+}
+
+func waitForBackupReady(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ah.APIClient)
+
+	stateRefreshFunc := func() (interface{}, string, error) {
+		backup, err := client.Backups.Get(context.Background(), d.Id())
+		if err != nil || backup == nil {
+			log.Printf("Error on waitForBackupReady: %v", err)
+			return nil, "", err
+		}
+		return backup.ID, backup.Status, nil
+	}
+
+	stateChangeConf := resource.StateChangeConf{
+		Delay:                     2 * time.Second,
+		Pending:                   []string{"queued", "saving"},
+		Refresh:                   stateRefreshFunc,
+		Target:                    []string{"active"},
+		Timeout:                   d.Timeout(schema.TimeoutUpdate),
+		MinTimeout:                2 * time.Second,
+		ContinuousTargetOccurence: 3,
+	}
+	_, err := stateChangeConf.WaitForState()
+
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for backup active status: %v", err)
+	}
+	return nil
+
+}
+
+func waitForBackupDestroy(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*ah.APIClient)
+
+	stateRefreshFunc := func() (interface{}, string, error) {
+		backup, err := client.Backups.Get(context.Background(), d.Id())
+		if err == ah.ErrResourceNotFound {
+			return d.Id(), "deleted", nil
+		}
+		if err != nil {
+			log.Printf("Error on waitForBackupDestroy: %v", err)
+			return nil, "", err
+		}
+
+		return backup.ID, backup.Status, nil
+	}
+
+	stateChangeConf := resource.StateChangeConf{
+		Delay:      5 * time.Second,
+		Pending:    []string{"pending_delete"},
+		Refresh:    stateRefreshFunc,
+		Target:     []string{"deleted"},
+		Timeout:    d.Timeout(schema.TimeoutUpdate),
+		MinTimeout: 2 * time.Second,
+	}
+	_, err := stateChangeConf.WaitForState()
+
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for backup to be destroyed: %s", err)
+	}
+
+	return nil
+}
---
+++ ah/resource_ah_cloud_server_snapshot_test.go
@@ -0,0 +1,184 @@
+package ah
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/advancedhosting/advancedhosting-api-go/ah"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccAHCloudServerSnapshot_Basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAHCloudServerSnapshotDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAHCloudServerSnapshotConfigBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("ah_cloud_server_snapshot.test", "id"),
+					resource.TestCheckResourceAttrPair("ah_cloud_server_snapshot.test", "cloud_server_id", "ah_cloud_server.web", "id"),
+					resource.TestCheckResourceAttrPair("ah_cloud_server_snapshot.test", "cloud_server_name", "ah_cloud_server.web", "name"),
+					resource.TestCheckResourceAttrSet("ah_cloud_server_snapshot.test", "state"),
+					resource.TestCheckResourceAttr("ah_cloud_server_snapshot.test", "name", "example-snapshot-1"),
+					resource.TestCheckResourceAttrSet("ah_cloud_server_snapshot.test", "size"),
+					resource.TestCheckResourceAttrSet("ah_cloud_server_snapshot.test", "type"),
+					resource.TestCheckResourceAttrSet("ah_cloud_server_snapshot.test", "created_at"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAHCloudServerSnapshot_UpdateName(t *testing.T) {
+	var beforeID, afterID string
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAHCloudServerSnapshotDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAHCloudServerSnapshotConfigBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAHCloudServerSnapshotExists("ah_cloud_server_snapshot.test", &beforeID),
+				),
+			},
+			{
+				Config: testAccCheckAHCloudServerSnapshotConfigUpdateName(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAHCloudServerSnapshotExists("ah_cloud_server_snapshot.test", &afterID),
+					resource.TestCheckResourceAttr("ah_cloud_server_snapshot.test", "name", "New Name"),
+					testAccCheckAHCloudServerSnapshotNoRecreated(t, &beforeID, &afterID),
+				),
+			},
+		},
+	})
+}
+
+func TestAccAHCloudServerSnapshot_UpdateCloudServer(t *testing.T) {
+	var beforeID, afterID string
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAHCloudServerSnapshotDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckAHCloudServerSnapshotConfigBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAHCloudServerSnapshotExists("ah_cloud_server_snapshot.test", &beforeID),
+					resource.TestCheckResourceAttrPair("ah_cloud_server_snapshot.test", "cloud_server_id", "ah_cloud_server.web", "id"),
+				),
+			},
+			{
+				Config: testAccCheckAHCloudServerSnapshotConfigUpdateCloudServer(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAHCloudServerSnapshotExists("ah_cloud_server_snapshot.test", &afterID),
+					resource.TestCheckResourceAttrPair("ah_cloud_server_snapshot.test", "cloud_server_id", "ah_cloud_server.web.1", "id"),
+					testAccCheckAHCloudServerSnapshotRecreated(t, &beforeID, &afterID),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAHCloudServerSnapshotExists(n string, BackupID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No volume attachment ID is set")
+		}
+
+		*BackupID = rs.Primary.ID
+		return nil
+	}
+}
+
+func testAccCheckAHCloudServerSnapshotNoRecreated(t *testing.T, beforeID, afterID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if *beforeID != *afterID {
+			t.Fatalf("Resource has been recreated, expected %s, got %s", *beforeID, *afterID)
+		}
+		return nil
+	}
+}
+
+func testAccCheckAHCloudServerSnapshotRecreated(t *testing.T, beforeID, afterID *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if *beforeID == *afterID {
+			t.Fatalf("Resource hasn't been recreated")
+		}
+		return nil
+	}
+}
+
+func testAccCheckAHCloudServerSnapshotDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*ah.APIClient)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "ah_cloud_server_snapshot" {
+			continue
+		}
+
+		_, err := client.Backups.Get(context.Background(), rs.Primary.ID)
+
+		if err != ah.ErrResourceNotFound {
+			return fmt.Errorf("Error removing backup %s", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
+
+func testAccCheckAHCloudServerSnapshotConfigBasic() string {
+	return fmt.Sprintf(`
+	resource "ah_cloud_server" "web" {
+	  name = "test"
+	  datacenter = "c54e8896-53d8-479a-8ff1-4d7d9d856a50"
+	  image = "f0438a4b-7c4a-4a63-a593-8e619ec63d16"
+	  product = "1a4cdeb2-6ca4-4745-819e-ac2ea99dc0cc"
+	}
+	
+	resource "ah_cloud_server_snapshot" "test" {
+	  cloud_server_id = ah_cloud_server.web.id
+	  name = "example-snapshot-1"
+	}`)
+}
+
+func testAccCheckAHCloudServerSnapshotConfigUpdateName() string {
+	return fmt.Sprintf(`
+	resource "ah_cloud_server" "web" {
+	  name = "test"
+	  datacenter = "c54e8896-53d8-479a-8ff1-4d7d9d856a50"
+	  image = "f0438a4b-7c4a-4a63-a593-8e619ec63d16"
+	  product = "1a4cdeb2-6ca4-4745-819e-ac2ea99dc0cc"
+	}
+	
+	resource "ah_cloud_server_snapshot" "test" {
+	  cloud_server_id = ah_cloud_server.web.id
+	  name = "New Name"
+	}`)
+}
+
+func testAccCheckAHCloudServerSnapshotConfigUpdateCloudServer() string {
+	return fmt.Sprintf(`
+	resource "ah_cloud_server" "web" {
+	  count = 2
+	  name = "test"
+	  datacenter = "c54e8896-53d8-479a-8ff1-4d7d9d856a50"
+	  image = "f0438a4b-7c4a-4a63-a593-8e619ec63d16"
+	  product = "1a4cdeb2-6ca4-4745-819e-ac2ea99dc0cc"
+	}
+	
+	resource "ah_cloud_server_snapshot" "test" {
+	  cloud_server_id = ah_cloud_server.web.1.id
+	  name = "example-snapshot-1"
+	}`)
+}
